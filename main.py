f009f58f01115272f5935e9195c18d37b8dda30c
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-10-05 18:51:00 -0300

    comment

--- main.py
+++ main.py
@@ -3,23 +3,30 @@ import copy
 
 class simplex:
     cntfolg = 1
+
+    #Reset Variables
     def reset():
         simplex.cntfolg = 1
 
+    #Transform a restriction into one in the standard form
     def standardize_r(inp):
         pos = 1
         newfrm = []
         for x in range(len(inp)):
             if isinstance(inp[x], str):
+
+                # Introducing new variables
                 if '>' in inp[x]: pos = -1
                 newfrm.extend([simplex.cntfolg+2,inp[x+1]*pos])
                 simplex.cntfolg+=1
+
                 for y in range(len(inp)-1):
                     if y == x: continue
                     newfrm.append(inp[y]*pos)
                 break
         return newfrm
 
+    #Transform the objective function into one in the standard form
     def standardize_f(inp):
         pos = 1
         newfrm = ["f", 0]
@@ -28,6 +35,7 @@ class simplex:
             newfrm.append(inp[x]*pos)
         return newfrm
 
+    #Transform the linear program to one in standard form
     def standardize(inp):
         fp = simplex.standardize_f(inp[0])
         sd = (["vnb", "ml"])
@@ -44,16 +52,19 @@ class simplex:
     def phase_1(inp):
         print("Phase 1")
         pos_fst = col_perm = -1
+
         for x in range(1, len(inp)):
             if inp[x][1] < 0:
                 print("Found: " + str(inp[x][1]))
                 pos_fst = x
                 break
 
+        # There is no negative row, what leads us to the second phase
         if pos_fst == -1:
             print ("TO PHASE 2")
             return -2
 
+        # Find the column for the feasible region
         for y in range(2, len(inp[pos_fst])):
             #print(inp[x][y])
             if inp[pos_fst][y] < 0:
@@ -61,10 +72,12 @@ class simplex:
                 col_perm = y
                 break
 
+        # There is no negative variable for the selected row, what characterizes an unfeasible solution
         if col_perm == -1:
-            print ("IMPOSSI")
+            print ("Infeasible")
             return -1
 
+        # Find the row for the feasible region
         mnr = float("inf")
         mnr_row = -1
         for x in range(1, len(inp)):
@@ -74,46 +87,63 @@ class simplex:
                 mnr = quo
                 mnr_row = x
         print (mnr_row)
+
         ew = simplex.troca(inp, mnr_row, col_perm)
+
+        # Replace old values for the new values
         for x in range(0, len(inp)):
             for y in range(0, len(inp[0])):
                 inp[x][y] = ew[x][y]
+
         simplex.printTable(inp)
         return 0
 
     def troca(inp, mnr_row, mnr_col):
+
         newtbl = copy.deepcopy(inp)
+
+        # Calculate and set new pivot
         elemnpr = 1/newtbl[mnr_row][mnr_col]
         print("lempr = " + str(elemnpr))
         newtbl[mnr_row][mnr_col] = elemnpr;
         simplex.printTable(newtbl)
+
+        # Calculate new values for the feasible row
         for x in range(1, len(inp)):
             if x == mnr_row: continue
             newtbl[x][mnr_col] = newtbl[x][mnr_col] * (-1*elemnpr)
         simplex.printTable(newtbl)
+
+        # Calculate new values for the feasible column
         for x in range(1, len(inp[0])):
             if x == mnr_col: continue
             newtbl[mnr_row][x] = newtbl[mnr_row][x] * (1*elemnpr)
         simplex.printTable(newtbl)
 
+        # Calculate new values for the rest of the table
         for x in range(1, len(inp)):
             for y in range(1, len(inp[0])):
                 if y == mnr_col: continue
                 if x == mnr_row: continue
                 newtbl[x][y] = inp[mnr_row][y] * newtbl[x][mnr_col]
 
-
+        # Switch column/row
         xi = newtbl[mnr_row][0]
         yi = newtbl[0][mnr_col]
         newtbl[mnr_row][0] = yi
         newtbl[0][mnr_col] = xi
+
+
         print("MULT222")
         simplex.printTable(newtbl)
+
+        # Calculate the sum for the new table
         for x in range(1, len(inp)):
             for y in range(1, len(inp[0])):
                 if x == mnr_row: continue
                 if y == mnr_col: continue
                 newtbl[x][y] += inp[x][y]
+
         print("ODN2")
         simplex.printTable(newtbl)
         #simplex.printTable(newtbl)
@@ -128,6 +158,8 @@ class simplex:
                 print("Found: " + str(inp[1][y]))
                 pos_fst = y
                 break
+
+        # There is no positive variable for the function row, what characterizes an optimal solution
         if pos_fst == -1:
             return -2
 
@@ -138,10 +170,12 @@ class simplex:
                 col_perm = pos_fst
                 break
 
+        # There is no negative variable for the selected row, what characterizes an unfeasible solution
         if col_perm == -1:
-            print ("ILIM")
+            print ("Unbounded")
             return -1
 
+        # Find the row for the feasible region
         mnr = float("inf")
         mnr_row = -1
         for x in range(1, len(inp)):
@@ -155,6 +189,8 @@ class simplex:
         #input()
         print("row = " + str(mnr_row) + " col = " + str(col_perm))
         ew = simplex.troca(inp, mnr_row, col_perm)
+
+        #Replace values
         for x in range(0, len(inp)):
             for y in range(0, len(inp[0])):
                 inp[x][y] = ew[x][y]
@@ -164,14 +200,28 @@ class simplex:
         return 0
         #return 0
 
+    def execute(stn):
+        rt = 0
+        while rt == 0:
+            rt = simplex.phase_1(stn)
+
+        if rt == -1:
+            print("DONE")
+        else:
+            rt = 0
+            while rt == 0:
+                rt = simplex.phase_2(stn)
+
+
+###TESTS
+
+
 #r1 =[8,2,'>=',16]
 #simplex.standardize_r(r1)
 
 #f = ["max",1,2]
 #simplex.standardize_f(f)
 
-
-###TESTS
 
 def test01():
     #numero 3 da lista de simplex
@@ -205,15 +255,6 @@ def test02():
     simplex.reset()
     stn = simplex.standardize(table)
     simplex.printTable(stn)
-    rt = 0
-    while rt == 0:
-        rt = simplex.phase_1(stn)
-
-    if rt == -1:
-        print("DONE")
-    else:
-        rt = 0
-        while rt == 0:
-            rt = simplex.phase_2(stn)
+    simplex.execute(stn)
 
 test02()
