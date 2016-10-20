c1943b53f6db6b00a3b2c7c28ef5ae014f1f76f3
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2016-10-20 15:42:50 -0200

    static folder

--- server.py
+++ server.py
@@ -2,8 +2,8 @@
 from flask import Flask, request, jsonify, render_template
 from simplex import Simplex, Result
 from json import dumps
-
-app = Flask(__name__, static_url_path='')
+app._static_folder = os.path.abspath("static/")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
